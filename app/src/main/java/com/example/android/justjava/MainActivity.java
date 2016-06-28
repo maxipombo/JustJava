8f18c1a6584effcf3b00f2803a55023787ca3d6a
Maximiliano Pombo <maxipombo@gmail.com>
2016-06-28 13:21:25 -0300

    Email Intent without delete the display message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -26,10 +29,10 @@ public class MainActivity extends ActionBarActivity {
     /**
      * Create a summary of the order
      *
-     * @param price of the order
+     * @param price           of the order
      * @param addWhippedCream is whether or not the user wants whipped cream topping
-     * @param addChocolate is whether or not the user wants chocolate
-     * @param name of the customer
+     * @param addChocolate    is whether or not the user wants chocolate
+     * @param name            of the customer
      * @return text summary
      */
 
@@ -56,24 +59,56 @@ public class MainActivity extends ActionBarActivity {
         EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
         displayMessage(priceMessage);
     }
 
     /**
      * Calculates the price of the order.
      *
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate    is whether or not the user wants chocolate
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Price of 1 cup of coffee
+        int basePrice = 5;
+
+        // Add $1 if the user wants whipped cream
+        if (addWhippedCream) {
+            basePrice += 1;
+        }
+
+        // Add $2 if the user wants chocolate
+        if (addChocolate) {
+            basePrice += 2;
+        }
+
+        // Calculate the total order price by multiplying by quantity
+        return quantity * basePrice;
     }
 
     /**
      * This method is called when the + is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -82,6 +117,13 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the - is clicked.
      */
     public void decrement(View view) {
+        // Show an error message as a toast
+        Toast.makeText(this, "You cannot have less than 0 coffees", Toast.LENGTH_SHORT).show();
+        // Exit this method early because there's nothing left to do
+        if (quantity == 1) {
+            return;
+        }
+
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
